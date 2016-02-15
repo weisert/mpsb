82c4113f4c1aa0e29a973836f2e04a3ba667230e
Igor Vayzert <igor.weisert@gmail.com>
2016-02-15 22:10:21 +0600

    Tests for mpsinfo module.

--- test/test_mpsinfo.py
+++ test/test_mpsinfo.py
@@ -4,5 +4,9 @@ import os
 
 
 def test_dict():
-    info = mpsinfo.read_info(os.path.join(common.TESTS, 'data', 'IMG.jpg'))
+    test_filename = os.path.join(common.TESTS, 'data', 'IMG.jpg')
+    info = mpsinfo.read_info(test_filename)
     assert 'size' in info
+    assert os.stat(test_filename).st_size == info['size']
+    assert 'md5' in info
+    assert info['md5'] == '1f92dd4c463ac03beb8b4838eb96e352'
