53be02d82cbc7592508613bfbfe4f4f1a7d55473
Igor Vayzert <igor.weisert@gmail.com>
2016-02-20 21:14:06 +0600

    Get rid of nose.

--- test/test_mpsinfo.py
+++ test/test_mpsinfo.py
@@ -15,15 +15,15 @@ def test_dict():
     test_filename = os.path.join(common.TESTS, 'data', 'IMG.jpg')
     info = mpsinfo.read_info(test_filename)
     assert 'EXIF ExifVersion' in info
-    assert 'LGE' == info['Image Make']
-    assert 'Nexus 5' == info['Image Model']
-    assert '2016:02:14 23:17:20' == info['datetime']
-    assert 720 == info['height']
-    assert '1f92dd4c463ac03beb8b4838eb96e352' == info['md5']
-    assert 'Horizontal (normal)' == info['orientation']
-    assert os.stat(test_filename).st_size == info['size']
-    assert 0 == round(info['timestamp'] - 1455470240, 5)
-    assert 1280 == info['width']
+    assert info['Image Make'] == 'LGE'
+    assert info['Image Model'] == 'Nexus 5'
+    assert info['datetime'] == '2016:02:14 23:17:20'
+    assert info['height'] == 720
+    assert info['md5'] == '1f92dd4c463ac03beb8b4838eb96e352'
+    assert info['orientation'] == 'Horizontal (normal)'
+    assert info['size'] == os.stat(test_filename).st_size
+    assert round(info['timestamp'] - 1455470240, 5) == 0
+    assert info['width'] == 1280
 
 
 def test_get_tags_error_handling():
@@ -35,8 +35,8 @@ def test_get_tags_error_handling():
 def test_get_tags():
     tags = mpsinfo.get_tags(common.TESTS,
                             os.path.join(common.TESTS, 'foo', 'bar', 'baz.jpg'))
-    assert 'baz.jpg' == tags['basename']
-    assert 'foo/bar' == tags['location']
-    assert 2 == len(tags['tags'])
+    assert tags['basename'] == 'baz.jpg'
+    assert tags['location'] == 'foo/bar'
+    assert len(tags['tags']) == 2
     assert u'foo' in tags['tags']
     assert u'bar' in tags['tags']
